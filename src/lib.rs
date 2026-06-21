fc2a2b2e0d1872138f9f3d9f8f1dfe481fb675a2
Matthew Landauer <mlandauer@gmail.com>
2026-06-21 11:49:33 +1000

    Add TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -128,6 +128,9 @@ pub const fn pin_state_for_name<const N: usize>(names: [&str; N], name: &str) ->
 #[macro_export]
 macro_rules! pin_log {
     ($name:literal) => {{
+        // TODO: Show a more helpful message if NAMES is not defined
+        // TODO: Rename NAMES to something a little more obscure / less likely to bump into an exisiting name
+        // TODO: Pass to a function that has the same parameters as the macro
         const PIN_STATE: usize = pin_logger::pin_state_for_name(NAMES, $name).unwrap();
         pin_logger::pin_log_internal(PIN_STATE, $name);
     }};
