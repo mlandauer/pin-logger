1ff40e9b152a4292edad7514df7301540faa80dd
Matthew Landauer <mlandauer@gmail.com>
2026-06-24 16:04:44 +1000

    Dumb multi threading again

--- examples/esp-embassy/src/main.rs
+++ examples/esp-embassy/src/main.rs
@@ -7,6 +7,9 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use core::cell::RefCell;
+
+use critical_section::Mutex;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
@@ -16,11 +19,15 @@ use esp_hal::{
     timer::timg::TimerGroup,
 };
 use log::info;
+use pin_logger::PinLogger;
 use pin_logger::pin_log;
 
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
+
+static MUTEX_PIN_LOGGER: Mutex<RefCell<Option<PinLogger<Output, 2>>>> =
+    Mutex::new(RefCell::new(None));
 
 #[allow(
     clippy::large_stack_frames,
@@ -40,16 +47,20 @@ async fn main(spawner: Spawner) -> ! {
 
     info!("Embassy initialized!");
 
-    let mut l = pin_logger::init2!([Output::new(
-        peripherals.GPIO25,
-        Level::Low,
-        Default::default(),
-    )]);
+    let l = pin_logger::init2!([
+        Output::new(peripherals.GPIO25, Level::Low, Default::default()),
+        Output::new(peripherals.GPIO32, Level::Low, Default::default()),
+    ]);
 
+    critical_section::with(|cs| MUTEX_PIN_LOGGER.borrow(cs).replace(Some(l)));
     spawner.spawn(task().unwrap());
 
     loop {
-        pin_log!(l, "Hello from the main loop");
+        critical_section::with(|cs| {
+            let mut borrow = MUTEX_PIN_LOGGER.borrow(cs).borrow_mut();
+            let l = borrow.as_mut().unwrap();
+            pin_log!(l, "Hello from the main loop");
+        });
         Timer::after(Duration::from_millis(500)).await;
     }
 }
@@ -57,7 +68,12 @@ async fn main(spawner: Spawner) -> ! {
 #[embassy_executor::task]
 async fn task() {
     loop {
-        info!("Hello from the task!");
+        critical_section::with(|cs| {
+            let mut borrow = MUTEX_PIN_LOGGER.borrow(cs).borrow_mut();
+            let l = borrow.as_mut().unwrap();
+            pin_log!(l, "Hello from the task!");
+        });
+        // info!("Hello from the task!");
         Timer::after(Duration::from_millis(700)).await;
     }
 }
