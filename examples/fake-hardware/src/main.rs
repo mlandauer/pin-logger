b1931a7af96cb938ffc56def2ec5a043f69f5e9f
Matthew Landauer <mlandauer@gmail.com>
2026-06-26 11:03:10 +1000

    Rename structs

--- examples/fake-hardware/src/main.rs
+++ examples/fake-hardware/src/main.rs
@@ -1,18 +1,18 @@
 // Simple example that doesn't depend on any particular embedded hardware that shows
 // how you can use this library
 
-use crate::test_pin::FakePin;
+use crate::test_pin::TestPin;
 use pin_logger::pin_log;
 
 mod test_pin;
 
-pin_logger::global_static!(FakePin);
+pin_logger::global_static!(TestPin);
 
 fn main() {
     colog::init();
 
     // We only need two output pins but we can pass more if we want
-    pin_logger::init!([FakePin::new(0), FakePin::new(1), FakePin::new(2)]);
+    pin_logger::init!([TestPin::new(0), TestPin::new(1), TestPin::new(2)]);
     pin_log!("Start");
     // Do something here
     pin_log!("Middle");
--- examples/fake-hardware/src/test_pin.rs
+++ examples/fake-hardware/src/test_pin.rs
@@ -1,39 +1,39 @@
 use embedded_hal::digital::{Error, ErrorKind, ErrorType, OutputPin};
 use log::info;
 
-pub(crate) struct FakePin {
+pub(crate) struct TestPin {
     pub(crate) number: u8,
 }
 
-impl FakePin {
+impl TestPin {
     pub(crate) fn new(number: u8) -> Self {
         Self { number }
     }
 }
 
 #[derive(Debug)]
-pub(crate) struct FakePinError;
+pub(crate) struct TestPinError;
 
-impl Error for FakePinError {
+impl Error for TestPinError {
     fn kind(&self) -> ErrorKind {
         ErrorKind::Other
     }
 }
 
-impl ErrorType for FakePin {
-    type Error = FakePinError;
+impl ErrorType for TestPin {
+    type Error = TestPinError;
 }
 
-pub(crate) type FakePinResult = Result<(), FakePinError>;
+pub(crate) type TestPinResult = Result<(), TestPinError>;
 
-impl OutputPin for FakePin {
-    fn set_low(&mut self) -> FakePinResult {
-        info!("==> Setting fake pin {} low", self.number);
+impl OutputPin for TestPin {
+    fn set_low(&mut self) -> TestPinResult {
+        info!("==> Setting test pin {} low", self.number);
         Ok(())
     }
 
-    fn set_high(&mut self) -> FakePinResult {
-        info!("==> Setting fake pin {} high", self.number);
+    fn set_high(&mut self) -> TestPinResult {
+        info!("==> Setting test pin {} high", self.number);
         Ok(())
     }
 }
